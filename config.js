490dbd48f0f0cd51d67fa2233d6b45c86d25d25c
Infos <atma@infogage.com>
2015-07-23 18:34:41 -0700

    0723

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	'port': process.env.PORT || 8080,
+	// 'database': 'mongodb://node:noder@novus.modulusmongo.net:27017/Iganiq8o',
+	'database': 'mongodb://Infoshare01:Kazumer01@ds047752.mongolab.com:47752/infosharedb',
+	'secret': 'ilovescotchscotchyscotchscotch'
+};
